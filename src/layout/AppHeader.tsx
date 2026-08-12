1a2bd50389119eb712fe0242717667a49cd30841
fl-souptik-dhar <souptik.dhar@flexiloans.com>
2026-08-12 18:21:57 +0530

    update

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -88,14 +88,14 @@ const AppHeader: React.FC = () => {
               width={154}
               height={32}
               className="dark:hidden"
-              src="./images/logo/logo.png"
+              src="/images/logo/logo.svg"
               alt="Logo"
             />
             <Image
               width={154}
               height={32}
               className="hidden dark:block"
-              src="./images/logo/logo-dark.png"
+              src="/images/logo/logo-dark.svg"
               alt="Logo"
             />
           </Link>
