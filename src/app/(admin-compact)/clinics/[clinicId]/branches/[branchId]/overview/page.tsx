d0c2c9440cb17dca618ee68337c1b02c1d8a1d40
fl-souptik-dhar <souptik.dhar@flexiloans.com>
2026-08-19 14:47:37 +0530

    update

--- src/app/(admin-compact)/clinics/[clinicId]/branches/[branchId]/overview/page.tsx
+++ src/app/(admin-compact)/clinics/[clinicId]/branches/[branchId]/overview/page.tsx
@@ -2,6 +2,7 @@ import BranchOverviewPanel from "@/components/branches/BranchOverviewPanel";
 import { Metadata } from "next";
 import React from "react";
 
+
 export const metadata: Metadata = {
   title: "Branch Overview | Jido Healthcare",
   description: "Summary of a branch's doctors, lab tests, and schedule",
